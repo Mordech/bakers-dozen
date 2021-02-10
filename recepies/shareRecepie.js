8952c0926376f4712ca1c2da6601c0b6ee69cfd4
Elad Mordechai Mizrahi <elad@mizrahi.cc>
2021-02-10 20:35:31 +0200

    Refactoring share recepie function

--- recepies/shareRecepie.js
+++ recepies/shareRecepie.js
@@ -2,25 +2,42 @@ import { Share } from "react-native";
 import store from "../redux/store";
 import { getIngredients, getInstructions } from "./makeRecepie";
 
-const sharableRecepie = () => {
-    const bake = store.getState().bake
-    const hydration = store.getState().hydration
-    const size = store.getState().size
-
-    const title = "*Recepie for " + size + " " + bake + " " + hydration.value + "% Hydration:*"
-    const ingredients = () =>
-        getIngredients(bake, hydration, size).map((item) =>
-            item.ingredient + ":" + Math.floor(item.value) + "\n").join('')
-
-    const instructions = () =>
-        getInstructions(bake).map((item, index) =>
-            index + 1 + '. ' + item + '\n').join('')
-
-    const footer = "Created by you with the help of the Baker's Dozen app"
-
-    return title + "\n\n*Ingredients:*\n" + ingredients() + '\n*Instructions:*\n' + instructions() + "\n\n" + footer
-};
-
 export default function shareRecepie() {
-    Share.share({ message: sharableRecepie() })
+    const bake = store.getState().bake;
+    const hydration = store.getState().hydration;
+    const size = store.getState().size;
+
+    const title = "Recepie for " + size + " " + bake + " " + hydration.value + "% Hydration:";
+
+    const ingredients =
+        getIngredients(bake, hydration, size).map((item) =>
+            `${item.ingredient}: ${Math.floor(item.value)} ${isLiquid(item)}
+`).join("");
+
+    const instructions =
+        getInstructions(bake).map((item, index) =>
+            `${index + 1}. ${item}
+`).join("");
+
+    const footer = "Created by you with the help of the Baker's Dozen app ";
+
+    const message =
+        `${title}
+        
+Ingredients:
+${ingredients}
+Instructions:
+${instructions}
+
+${footer}`;
+
+    Share.share({ message })
+
+    //Check if the ingredient is water/olive oil or not
+    function isLiquid(item) {
+        return item.ingredient.includes("Water") ||
+            item.ingredient.includes("Olive")
+            ? "ml"
+            : "g";
+    }
 }
