663d512503e88d606da6fdad0b4d68655c8b987d
Elad Mordechai Mizrahi <elad@mizrahi.cc>
2021-02-12 19:21:02 +0200

    style: Move commas to the end of the line in pretzel instructions

--- recepies/recepies.js
+++ recepies/recepies.js
@@ -145,32 +145,19 @@ function makePretzel(hydration, size) {
 }
 
 const pretzelInsctructions = [
-    "In your measuring cup of lukewarm water, add the yeast and sugar. Allow the yeast to activate for about 5 minutes, or until foamy."
-    ,
-    "In your stand mixer bowl, combine the flour and salt. Add in the water with activated yeast and the cooled, melted butter."
-    ,
-    "Mix using the dough hook attachment on low speed for 2-3 minutes until the dough forms. Knead with your dough hook on medium speed for 5-6 minutes."
-    ,
-    "Remove the dough from the bowl (or transfer to an oiled bowl) long enough to oil the bowl with olive oil. Roll the dough through the oil to coat all sides. Cover with a kitchen towel or plastic wrap.  Allow the dough to rise for 1 hour, or until doubled in size."
-    ,
-    "Punch the risen dough down about half way, then remove from the bowl and transfer to your floured working surface. Knead the bread for 2 minutes, then divide into 10 equal portions."
-    ,
-    "Make sure to dust your hands with some flour before rolling the portion balls of dough in your hands with the smooth rounded side facing up and the bottom side cupped in one hand. Use your free hand to smooth the domed top portion of the dough ball, making the surface less likely to lose shape. Repeat with each bun."
-    ,
-    "Cover your shaped buns with your kitchen towel or plastic wrap and allow them to rise for 20-30 minutes, they should almost double in size again."
-    ,
-    "Preheat your oven to 220 degrees C and line a baking sheet with parchment paper."
-    ,
-    "Bring a large stock pot with 4 cups of water and the baking soda to a boil."
-    ,
-    "Lower the pretzel buns into the boiling water using a slotted spoon, place as many into the boiling water as you can fit into your stock pot without crowding them as you do not want them touching. Also note that you should have enough water in the stock pot to prevent the buns from touching the bottom as well."
-    ,
-    "Boil each of the buns for 1 minute, starting with the rounded side up and turning halfway through the minute (30 seconds each side). Use your slotted spoon to remove the buns after a minute and place them onto the parchment paper lined baking sheet."
-    ,
-    "Score the tops of each bun using a sharp knife. You can do the same three slits as I have, or the usual pretzel cross that is common. Whatever shape or design you like. Score each bun as it comes out of the boiling water, then top with coarse sea salt."
-    ,
-    "Bake for 15 minutes or until golden brown and the buns are still soft on the crust, but they are not too soft. Your finger indent should spring back, rather than leaving a mark."
-    ,
+    "In your measuring cup of lukewarm water, add the yeast and sugar. Allow the yeast to activate for about 5 minutes, or until foamy.",
+    "In your stand mixer bowl, combine the flour and salt. Add in the water with activated yeast and the cooled, melted butter.",
+    "Mix using the dough hook attachment on low speed for 2-3 minutes until the dough forms. Knead with your dough hook on medium speed for 5-6 minutes.",
+    "Remove the dough from the bowl (or transfer to an oiled bowl) long enough to oil the bowl with olive oil. Roll the dough through the oil to coat all sides. Cover with a kitchen towel or plastic wrap.  Allow the dough to rise for 1 hour, or until doubled in size.",
+    "Punch the risen dough down about half way, then remove from the bowl and transfer to your floured working surface. Knead the bread for 2 minutes, then divide into 10 equal portions.",
+    "Make sure to dust your hands with some flour before rolling the portion balls of dough in your hands with the smooth rounded side facing up and the bottom side cupped in one hand. Use your free hand to smooth the domed top portion of the dough ball, making the surface less likely to lose shape. Repeat with each bun.",
+    "Cover your shaped buns with your kitchen towel or plastic wrap and allow them to rise for 20-30 minutes, they should almost double in size again.",
+    "Preheat your oven to 220 degrees C and line a baking sheet with parchment paper.",
+    "Bring a large stock pot with 4 cups of water and the baking soda to a boil.",
+    "Lower the pretzel buns into the boiling water using a slotted spoon, place as many into the boiling water as you can fit into your stock pot without crowding them as you do not want them touching. Also note that you should have enough water in the stock pot to prevent the buns from touching the bottom as well.",
+    "Boil each of the buns for 1 minute, starting with the rounded side up and turning halfway through the minute (30 seconds each side). Use your slotted spoon to remove the buns after a minute and place them onto the parchment paper lined baking sheet.",
+    "Score the tops of each bun using a sharp knife. You can do the same three slits as I have, or the usual pretzel cross that is common. Whatever shape or design you like. Score each bun as it comes out of the boiling water, then top with coarse sea salt.",
+    "Bake for 15 minutes or until golden brown and the buns are still soft on the crust, but they are not too soft. Your finger indent should spring back, rather than leaving a mark.",
     "Remove from the oven when done and transfer to a wire cooling rack to cool completely."
 ];
 
